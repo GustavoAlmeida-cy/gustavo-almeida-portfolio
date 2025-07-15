840cfde422a80db1cdd8549cbf64adb8c703da81
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2025-07-15 16:49:14 -0300

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="px-4">{children}</main>
       </body>
     </html>
   );
