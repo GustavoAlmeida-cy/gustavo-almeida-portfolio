68d297d56c3c501735c61170e2b45ecc000cf3e6
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2025-07-15 22:56:48 -0300

    footer icons

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="flex justify-center items-center p-6">
       <div className="flex items-center gap-4">
-        <Link href="#">Github</Link>
-        <Link href="#">Linkedin</Link>
-        <Link href="#">Whatsapp</Link>
+        <Link href="#" className="flex items-center gap-2">
+          <FaGithub size={24} />
+          <span>Github</span>
+        </Link>
+        <Link href="#" className="flex items-center gap-2">
+          <FaLinkedin size={24} />
+          <span>Linkedin</span>
+        </Link>
+        <Link href="#" className="flex items-center gap-2">
+          <FaWhatsapp size={24} />
+          <span>Whatsapp</span>
+        </Link>
       </div>
     </footer>
   );
